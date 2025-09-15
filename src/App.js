1f86c726ac82c7488603f8246be3a85dc8d5d5e8
hanjuseung <rhdqn0501@naver.com>
2025-09-15 16:03:12 +0900

    포트폴리오 배포 경로 설정6

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import './App.css';
-import { BrowserRouter,Route,Routes } from "react-router-dom";
+import { BrowserRouter,HashRouter,Route,Routes } from "react-router-dom";
 import MainPage from './main/MainPage';
 import Project1 from './project/Project1';
 import Project2 from './project/Project2';
@@ -7,14 +7,14 @@ import Project3 from './project/Project3';
 
 function App() {
   return (
-    <BrowserRouter basename={process.env.PUBLIC_URL}>
+    <HashRouter>
       <Routes>
         <Route path="/" element={<MainPage/>} />
         <Route path="/project1" element={<Project1/>} />
         <Route path="/project2" element={<Project2/>} />
         <Route path="/project3" element={<Project3/>} />
       </Routes>
-    </BrowserRouter>
+    </HashRouter>
   );
 }
 
