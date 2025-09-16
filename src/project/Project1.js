81005c0cd9f1412b697cbd67fb75bf5a01a1786f
hanjuseung <rhdqn0501@naver.com>
2025-09-16 09:18:52 +0900

    1차 프로젝트 페이지

--- src/project/Project1.js
+++ src/project/Project1.js
@@ -8,23 +8,15 @@ function Project1() {
     return (
         <div className="container1">
             <Header />
-            <h1>1차 프로젝트</h1>
-
-            <video
-                src={process.env.PUBLIC_URL + "/videos/project1.mp4"}    // public 기준 절대경로
-                controls                 // 재생 컨트롤 표시
-                autoPlay                 // 자동 재생 (옵션)
-                muted                    // 자동 재생 시 필수
-                loop                     // 반복 재생 (옵션)
-                width="850"              // 원하는 크기
-            ></video>
+            <h1>커뮤니티 기반 클라우드 펀딩 사이트</h1>
 
             <section className="project-overview">
-                <h2>커뮤니티 기반 클라우드 펀딩 사이트</h2>
+                <h2>프로젝트 개요</h2>
+                <p className="project1-p">기존 크리우드 펀딩 사이트 에서는 구매자와 판매자의 소통창구가 부족하다고 느꼈습니다. <br/> 크라우드 펀딩은 펀딩 성공 여부로 제품을 받기
+                    때문에 상품을 구매하고 펀딩이 성공할 때까지 과정을 알 수 있는 구매자와 판매자의 소통 공간의 필요성을 느껴 게시판 기능이 있는 커뮤니티 크라우드 펀딩 사이트를 구현해보았습니다. </p>
                 <ul>
                     <li>기간 : 2025.05.28 ~ 2025.06.13</li>
                     <li>인원 : 4명</li>
-                    <li>내 역할 : 로그인,마이페이지 및 메인페이지 구현</li>
                 </ul>
                 <div className="links">
                     <a href="https://github.com/hanjuseung/funders" target="_blank">GitHub 주소</a>
@@ -53,11 +45,53 @@ function Project1() {
                 </div>
             </section>
 
+            <section>
+                <h2>프로젝트 일정</h2>
+                <div>
+                    <img src={process.env.PUBLIC_URL + "/images/WBS1.png"} alt="프로젝트일정" className="portfolio1-img"></img>
+                </div>
+            </section>
+
+            <section>
+                <h2>요구사항 명세서</h2>
+                <img src={process.env.PUBLIC_URL + "/images/Requirements1.png"} alt="요구사항명세서" style={{ width: '850px', objectFit: 'contain' }}></img>
+            </section>
+
+            <section>
+                <h2>메뉴트리</h2>
+                <div>
+                    <img src={process.env.PUBLIC_URL + "/images/Menutree.png"} alt="메뉴트리" style={{ width: '850px', height: '350px', objectFit: 'contain' }}></img>
+                </div>
+            </section>
+
+            {/* <section>
+                <h2>ERD</h2>
+                <div></div>
+            </section> */}
+
+            <section>
+                <h2>영상 시연</h2>
+                <video
+                    src={process.env.PUBLIC_URL + "/videos/project1.mp4"}    // public 기준 절대경로
+                    controls                 // 재생 컨트롤 표시
+                    autoPlay                 // 자동 재생 (옵션)
+                    muted                    // 자동 재생 시 필수
+                    loop                     // 반복 재생 (옵션)
+                    width="850"              // 원하는 크기
+                ></video>
+            </section>
+
             <section className="project-result">
-                <h2>프로젝트를 통해 배운 점</h2>
+                <h2>프로젝트를 통해 느낀 점, 배운 점</h2>
                 <p>
-                    React SPA 구조와 상태 관리를 직접 구현하면서 컴포넌트 분리와
-                    재사용성의 중요성을 이해했고, Git 브랜치 전략을 통한 협업 방법을 익혔습니다.
+                    제가 맡은 부분은 로그인, 회원가입창과 마이페이지였습니다.
+                    React만으로 제작한 첫 프로젝트라서 기초 세팅부터 라우팅, 상태 관리까지 모든 과정을 직접 구현해야 했습니다.
+                    특히 React SPA 구조와 상태 관리를 직접 구현하면서 컴포넌트 분리와 재사용성의 중요성을 깊이 이해했고,
+                    Props와 State를 어떻게 설계해야 유지보수가 쉬운지 체감했습니다.
+                    또한 Git 브랜치를 활용해 팀원들과 병렬 개발을 진행하며
+                    코드 충돌을 최소화하고 협업 효율을 높이는 방법을 익혔습니다.
+                    이 경험을 통해 단순히 기능을 완성하는 것뿐 아니라
+                    코드 품질과 협업 프로세스가 프로젝트 성공에 얼마나 큰 영향을 미치는지 배우게 되었습니다.
                 </p>
             </section>
 
